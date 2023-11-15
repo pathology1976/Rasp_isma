0d6e3b679799a660c7db9c0a2b6896da41e04611
pathology1976 <72729724+pathology1976@users.noreply.github.com>
2023-11-15 19:11:35 +0400

    Смена кода

--- Rasp1.py
+++ Rasp1.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from datetime import datetime
-import arrow 
 
 current_datetime = datetime.now()
 current_day = current_datetime.strftime("%A")
-current_time = now.format("HH:mm:ss")
+current_time = datetime.now().time()
+time_in_minutes = (current_time.hour * 60) + current_time.minute
 print(current_day)
 print(current_time)
 
@@ -13,8 +13,8 @@ def idet_para(rasp):
     current_lesson = rasp[
     (rasp['group'] == group) & 
     (rasp['day'] == current_day) & 
-    (rasp['start_time'] <= (current_time)) & 
-    (rasp['finish_time'] >= (current_time))]
+    (rasp['start_time'].astype(int) <= (time_in_minutes)) & 
+    (rasp['finish_time'].astype(int) >= (time_in_minutes))]
     
     if not current_lesson.empty:
         lesson = current_lesson.iloc[0]["lesson"]
